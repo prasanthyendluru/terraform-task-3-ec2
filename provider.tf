fd990310717d96a858216a9d555968ad6cd9ac12
prasanthyendluru <yendluruprasanth@gmail.com>
2025-08-21 10:13:09 +0530

    final

--- provider.tf
+++ provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  #   backend "s3" {
+  #     bucket       = "prasanth-devops-nation-s3-20082024"
+  #     key          = "prasanth-terraform.tfstate"
+  #     region       = "eu-west-2"
+  #     use_lockfile = true
+  #   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# provider "aws" {
+#   region = "eu-west-2"
+# }
+
 provider "aws" {
   region = "eu-west-2" # Change to your assigned region
 }
